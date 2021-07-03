ca62a71b376e59ab17c923ee7b7aa624b4ef455c
William Johnson <wlpj2011@gmail.com>
2021-07-03 19:43:52 -0400

    Solution to problem 9

---
+++ problem0009.py
@@ -0,0 +1,13 @@
+import sys
+
+def solution(n):
+    print(n)
+    for i in range(1,n+1):
+        for j in range(1,n+1-i):
+            k = n-i-j
+            if i*i+j*j==k*k:
+                return i*j*k
+
+if __name__=="__main__":
+    n = int(sys.argv[1])
+    print("The solution is: " + str(solution(n)))
