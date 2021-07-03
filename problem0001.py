5225539989b11a5b227d60e10fbd313a8c1e7fb5
William Johnson <wlpj2011@gmail.com>
2021-07-03 17:42:26 -0400

    Solution to Problem 1

---
+++ problem0001.py
@@ -0,0 +1,13 @@
+import sys
+
+def solution(n):
+    result = 0
+    for i in range(1,n):
+        if i%3 == 0 or i%5 == 0:
+            result += i
+
+    return result
+
+if __name__ == "__main__":
+    n = int(sys.argv[1])
+    print(solution(n))
