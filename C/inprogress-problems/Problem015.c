c5e60ec28ab6d757ec6280e5e9dbb7065882d568
William Johnson <wlpj2011@gmail.com>
2023-09-17 17:57:52 -0400

    Worked on 015C
    Tried 015C the easy way, but current method of computing binomial coefficients doesn't fit inside a long without overflow.

--- C/inprogress-problems/Problem015.c
+++ C/inprogress-problems/Problem015.c
@@ -4,8 +4,9 @@
 #include <math.h>
 #include <time.h>
 
-int sol_P015(int n);
+long sol_P015(int n);
 
+long binom(int n, int k);
 
 int main(int argc, char *argv[]){
     if(argc != 2){
@@ -16,15 +17,37 @@ int main(int argc, char *argv[]){
     double cpu_time_used;
     int n = atoi(argv[1]);
     start = clock();
-    int solution = sol_P015(n);
+    long solution = sol_P015(n);
     end = clock();
-    printf("The solution to %s is %i which took %f seconds to compute.\n", argv[0], solution, ((double) (end - start))/CLOCKS_PER_SEC);
+    printf("The solution to %s is %li which took %f seconds to compute.\n", argv[0], solution, ((double) (end - start))/CLOCKS_PER_SEC);
     exit(0);
 }
 
-int sol_P015(int n){
+long sol_P015(int n){
     // Finds the number of paths moving only down and right from the top left to the bottom right of an n x n grid.
-    int result = 0;
-    
+    long result = 0;
+    result = binom(2*n, n);
+    return result;
+}
+
+long binom(int n, int k){
+    // Computer n choose k i.e. n!/((n-k)!k!)
+    if(k > n){
+        return 0;
+    }
+    if((k==0) || (k == n)){
+        return 1;
+    }
+    unsigned long result = 1;
+    for (long i = k+1; i < n+1; i++)
+    {
+        result *=  i;
+    }
+    for (long i = 1; i < n - k + 1; i++)
+    {
+        result /= i;
+    }
+    printf("%li\n", result);
+    printf("%lu\n", sizeof(long long));
     return result;
 }
