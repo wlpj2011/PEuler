7ceb3a852579a7c5465268ba27c7a36ae650e015
William Johnson <wlpj2011@gmail.com>
2023-09-24 10:41:43 -0400

    worked on 018C
    Should be able to read in and store the triangle from the file, but isn't working for some reason. It just prints a triangle of 0s instead of the triangle from the file.

--- C/inprogress-problems/Problem018.c
+++ C/inprogress-problems/Problem018.c
@@ -25,7 +25,39 @@ int main(int argc, char *argv[]){
 int sol_P018(int n){
     // Finds the number of letters used to write out all the numbers from 1 to n
     int result = 0;
+    FILE *fptr;
 
-    
+    fptr = fopen("/Users/William/Desktop/PEuler/TextInputs/Problem018.txt","r");
+
+    if(fptr == NULL) {
+        printf("Not able to open Problem018.txt.\n");
+        exit(1);
+    }
+    rewind(fptr);
+
+    int *digits[n];
+    for (int i = 0; i < n; i ++){
+        digits[i] = malloc((i + 1) * sizeof(int));
+    }
+
+
+    fclose(fptr);
+    for (int i = 0; i < n; i ++){
+        for (int j = 0; j < i; j++){
+            fscanf(fptr, "%2d", &digits[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i ++){
+        for (int j = 0; j < i; j++){
+            printf("%2d ", digits[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        free(digits[i]);
+    }
     return result;
 }
