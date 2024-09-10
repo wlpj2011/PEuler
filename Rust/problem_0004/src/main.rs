d6fb0dafae0c9d7339c5c9a776d01570f46924f1
William Johnson <wlpj2011@gmail.com>
2024-09-10 18:46:53 -0400

    Rust: Started working on problem 4

--- Rust/problem_0004/src/main.rs
+++ Rust/problem_0004/src/main.rs
@@ -5,19 +5,25 @@ fn main() {
     loop {
         println!("Please input your number of digits.");
 
-        let mut num_digts = String::new();
+        let mut num_digits = String::new();
 
         io::stdin()
-            .read_line(&mut num_digts)
+            .read_line(&mut num_digits)
             .expect("Failed to read line");
 
-        let num_digts: u32 = match num_digts.trim().parse() {
+        let num_digits: u32 = match num_digits.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
         //let result = mul_3_5_sum(upper_bound);
         //println!("The result is {result}");
+        //is_palindrome(num_digits);
         break;
     }
+}
+
+fn is_palindrome(number: u32) -> bool {
+    let num_digits = ((number as f32) + 0.1).log10().ceil() as u32;
+    true 
 }
 
